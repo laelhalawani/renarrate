cb73c8666a94539d46058aee87658a50cee29b52
laelhalawani <lael.alhalawani@piercegroup.com>
2025-08-09 20:15:13 +0200

    comment out gemini voice seletion since we're using elevenlabs in the example

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ if __name__ == "__main__":
     # Example usage
     video_url = "https://www.youtube.com/watch?v=imiAXNnzkAc"
     target_language = select_language_by_name("Polish")
-    gemini_voice = select_gemini_voice("Orus")
+    # gemini-tts is in preview mode, it has really limited rates on Tier 1 only 10 RPM, 
+    # the system will be fine (will retry with 30sec delay for each fragment generation up to 5 times, but will be really slow)
+    # uncomment below if you want to try
+    # gemini_voice = select_gemini_voice("Orus") 
     elevenlabs_voice = select_elevenlabs_voice("Daniel")
     paths = run_pipeline(video_url, target_language, elevenlabs_voice)
     convert_video(paths.downloaded_video_path, "mp4")
